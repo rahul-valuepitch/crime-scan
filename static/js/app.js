164445b14029d02d737310ba270b58b0c2b7b656
Rahul <rahul.yadav@valuepitch.com>
2024-04-30 12:11:10 +0530

    Added Video

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 AOS.init({
   easing: "ease",
   once: true,
-  duration: 500,
+  duration: 1000,
 });
 
 // Copyright
@@ -12,3 +12,55 @@ const copyright = () => {
   tag.textContent = year;
 };
 copyright();
+
+// Counter
+const counter = (selector) => {
+  var counted = 0;
+  $(window).scroll(function () {
+    var oTop = $(selector).offset().top - window.innerHeight;
+    if (counted == 0 && $(window).scrollTop() > oTop) {
+      $(".count").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+          {
+            duration: 3000,
+            easing: "swing",
+            step: function () {
+              var formattedNum = Math.floor(this.countNum);
+              if (formattedNum < 10) {
+                formattedNum = "0" + formattedNum;
+              }
+              $this.text(formattedNum);
+            },
+            complete: function () {
+              var finalNum = Math.floor(this.countNum);
+              if (finalNum < 10) {
+                finalNum = "0" + finalNum;
+              }
+              $this.text(finalNum);
+            },
+          }
+        );
+      });
+      counted = 1;
+    }
+  });
+};
+
+// Play Video
+const playVideo = () => {
+  const playBtn = document.querySelector(".video-content");
+  const video = document.querySelector(".video-content video");
+
+  playBtn.addEventListener("click", () => {
+    video.play();
+    playBtn.classList.add("active");
+  });
+};
+playVideo();
